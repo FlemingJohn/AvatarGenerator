1bf31599819a621975d29d7277f74bee3eb3b9d7
Fleming John <flemjohn08@gmail.com>
2025-07-02 23:08:56 +0530

    new changes made

--- CluelessGenerator/script.js
+++ CluelessGenerator/script.js
@@ -1,3 +1,14 @@
-document.getElementById("random-btn").addEventListener("click", () =>{
-    console.log("Randomize button clicked !");
-});
+const categories = {
+    addOns:{
+        images:[
+      "https://cdn.glitch.global/61b7b4da-9ad2-4a9f-abea-326808d0ac33/21.png",
+      "https://cdn.glitch.global/61b7b4da-9ad2-4a9f-abea-326808d0ac33/22.png",
+      "https://cdn.glitch.global/61b7b4da-9ad2-4a9f-abea-326808d0ac33/23.png",
+      "https://cdn.glitch.global/61b7b4da-9ad2-4a9f-abea-326808d0ac33/24.png",
+      "https://cdn.glitch.global/61b7b4da-9ad2-4a9f-abea-326808d0ac33/25.png",
+        ],
+        index: 0,
+        element: document.getElementById("addOns"),
+        next-btn: ""
+    }
+}
